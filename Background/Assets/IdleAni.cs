0c9443bc329706d547d5ae4cfd87ddc782118dc9
Jun Hong <junhong@umail.ucsb.edu>
2019-02-12 18:45:11 -0800

    updated background visuals

---
+++ Background/Assets/IdleAni.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IdleAni : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent <Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            anim.SetTrigger("Active");
+
+        if (Input.GetMouseButton(1))
+            anim.SetTrigger("BarkActive");
+    }
+}
