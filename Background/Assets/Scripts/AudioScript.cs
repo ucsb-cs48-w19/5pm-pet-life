7e7ce893205cfd4ae3dc3c93a07170c3f54a8146
chenyinluo <c_luo@umail.ucsb.edu>
2019-02-14 18:52:42 -0800

    Change curson

---
+++ Background/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip BackgoundMusic;
+
+    public AudioSource MusicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = BackgoundMusic; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MusicSource.Play();
+    }
+}
