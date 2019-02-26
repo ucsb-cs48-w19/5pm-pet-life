d1101412d83629591ede4942d97a8cbe86b98c77
unknown <jonathanxu@ucsb.edu>
2019-02-25 17:05:48 -0800

    Modified install/build scripts, attempt to resolve build issues

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,7 +9,7 @@ echo "Attempting to build $project for Windows"
   -silent-crashes 
   -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+  -buildWindows64Player "$(pwd)/Build/windows/$project.exe" 
   -quit
 
 echo "Attempting to build $project for OS X"
