2a17631ce78588e35d6af2106568a8706d82f3c5
unknown <jonathanxu@ucsb.edu>
2019-02-27 17:19:15 -0800

    Added basic test to check for dog object's existance

---
+++ Background/Assets/Tests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Tests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+        [Test]
+        public void getDog() {
+            GameObject dog;
+            dog = GameObject.Find("Dog_0");
+            Assert.AreEqual(dog, GameObject.Find("Dog_0"));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
