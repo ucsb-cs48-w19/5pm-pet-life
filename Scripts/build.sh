6643bb4f0c45dd3ea2f39345d34f5004ae78d8b2
unknown <jonathanxu@ucsb.edu>
2019-02-27 22:04:25 -0800

    removed some lines in the build.sh script since we don't need to zip up the actual unity builds

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -34,9 +34,3 @@ echo "Attempting to build $project for Linux"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
